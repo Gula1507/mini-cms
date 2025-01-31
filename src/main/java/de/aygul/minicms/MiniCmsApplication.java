8b855258a02e70eaa775f632d47ef3f44252f3b7
Aygul <gulya@gmx.net>
2025-01-31 11:03:31 +0100

    initialize project structure

---
+++ src/main/java/de/aygul/minicms/MiniCmsApplication.java
@@ -0,0 +1,13 @@
+package de.aygul.minicms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniCmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniCmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/aygul/minicms/MiniCmsApplicationTests.java
@@ -0,0 +1,17 @@
+package de.aygul.minicms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniCmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+        /*
+         * This method is intentionally empty.
+         * It is used to check if the Spring ApplicationContext loads successfully.
+         */
+    }
+
+}
