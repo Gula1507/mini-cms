d5912be6b2ca1cbc246f4b0086151cc212109e8d
Aygul <gulya@gmx.net>
2025-02-01 23:25:26 +0100

    fix: add @DirtiesContext to controller tests

--- src/test/java/de/aygul/minicms/controller/BlogPostControllerTest.java
+++ src/test/java/de/aygul/minicms/controller/BlogPostControllerTest.java
@@ -23,7 +23,7 @@ import java.util.List;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
