41b342c2671e336650637aae7621b1152c2fe3b6
Aygul <gulya@gmx.net>
2025-02-01 20:45:09 +0100

    feat: change createBlog to take BlogPostDto and return Long instead of BlogPost; implemented ApplicationMediator, updated related tests

--- src/test/java/de/aygul/minicms/controller/BlogPostControllerTest.java
+++ src/test/java/de/aygul/minicms/controller/BlogPostControllerTest.java
@@ -1,8 +1,7 @@
 package de.aygul.minicms.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import de.aygul.minicms.model.BlogPost;
-import de.aygul.minicms.model.BlogPostStatus;
+import de.aygul.minicms.model.*;
 import de.aygul.minicms.repository.BlogPostRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,8 +13,11 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import java.time.LocalDateTime;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -34,23 +36,25 @@ class BlogPostControllerTest {
     ObjectMapper objectMapper;
 
     @Test
-    @DisplayName("createBlog save BlogPost with Draft status")
+    @DisplayName("createBlog return status 201 and generated ID")
     void createBlog() throws Exception {
+        BlogPostDTO blogPostDTO = new BlogPostDTO("Valid Title",
+                "Valid Body",
+                "Valid Author",
+                List.of(new CategoryDTO("Tech")));
+
         BlogPost blogPost = new BlogPost(null,
-                "Sample Blog Post Title",
-                "This is the content of the blog post.",
-                "Author Name",
-                LocalDateTime.now(),
-                BlogPostStatus.PUBLISHED,
-                List.of());
+                "Valid Title",
+                "Valid Body",
+                "Valid Author",
+                LocalDate.now(),
+                null,
+                List.of(new Category(null, "Tech", new ArrayList<>())));
 
         blogPostRepository.save(blogPost);
-
         mockMvc.perform(MockMvcRequestBuilders.post("/blogpost").contentType(MediaType.APPLICATION_JSON)
-                                              .content(objectMapper.writeValueAsString(blogPost)))
-               .andExpect(status().isCreated()).andExpect(jsonPath("$.title").value("Sample Blog Post Title"))
-               .andExpect(jsonPath("$.id").isNotEmpty())
-               .andExpect(jsonPath("$.body").value("This is the content of the blog post."))
-               .andExpect(jsonPath("$.author").value("Author Name")).andExpect(jsonPath("$.blogStatus").value("DRAFT"));
+                                              .content(objectMapper.writeValueAsString(blogPostDTO)))
+               .andExpect(status().isCreated()).andExpect(jsonPath("$").isNumber());
+
     }
 }
