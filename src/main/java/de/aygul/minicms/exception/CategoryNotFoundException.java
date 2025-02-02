022f3a763b4abc7eb23fa71946536186d8b0d0e4
Aygul <gulya@gmx.net>
2025-02-02 16:48:27 +0100

    chore: add space in message

--- src/main/java/de/aygul/minicms/exception/CategoryNotFoundException.java
+++ src/main/java/de/aygul/minicms/exception/CategoryNotFoundException.java
@@ -2,6 +2,6 @@ package de.aygul.minicms.exception;
 
 public class CategoryNotFoundException extends RuntimeException {
     public CategoryNotFoundException(String message) {
-        super("The category is not found."+ message );
+        super("The category is not found. "+ message );
     }
 }
