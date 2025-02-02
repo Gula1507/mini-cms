900cacb423aaf775c0cf63f201c80e75d130aa1f
Aygul <gulya@gmx.net>
2025-02-02 22:36:33 +0100

    fix: enable case-insensitive deserialization for BlogPostStatus

--- src/main/java/de/aygul/minicms/model/BlogPostStatus.java
+++ src/main/java/de/aygul/minicms/model/BlogPostStatus.java
@@ -1,5 +1,14 @@
 package de.aygul.minicms.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public enum BlogPostStatus {
-    DRAFT, PUBLISHED, ARCHIVED
-}
+    DRAFT, PUBLISHED, ARCHIVED;
+
+    @JsonCreator
+    public static BlogPostStatus fromString(@JsonProperty("blogpoststatus") String value) {
+        return BlogPostStatus.valueOf(value.toUpperCase());
+    }
+    }
+
