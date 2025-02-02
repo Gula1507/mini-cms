77c51042e72138f65b20dead810eff4dc27c178f
Aygul <gulya@gmx.net>
2025-02-02 12:39:13 +0100

    feat: add GlobalExceptionHandler and ErrorMessage

---
+++ src/main/java/de/aygul/minicms/exception/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package de.aygul.minicms.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(BlogPostIdNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorMessage handleBlogPostNotFound(BlogPostIdNotFoundException ex) {
+        return new ErrorMessage(ex.getMessage());
+    }
+}
