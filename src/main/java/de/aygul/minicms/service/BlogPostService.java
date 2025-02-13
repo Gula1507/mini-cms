512db2740ea2608bcfb1df051719780e5e6c1b42
Aygul <gulya@gmx.net>
2025-02-13 12:11:16 +0100

    refactor: change access modifier to private for helper methods and move them to the end of the class

--- src/main/java/de/aygul/minicms/service/BlogPostService.java
+++ src/main/java/de/aygul/minicms/service/BlogPostService.java
@@ -26,7 +26,6 @@ public class BlogPostService {
 
     public Long createBlogPost(BlogPostRequestDTO blogPostRequestDTO) {
         BlogPost blogPost = convertToEntity(blogPostRequestDTO);
-
         blogPostRepository.save(blogPost);
         return blogPost.getId();
     }
@@ -58,61 +57,6 @@ public class BlogPostService {
         return convertToResponseDTO(updatedBlogPost);
     }
 
-    public BlogPostHistory convertBlogPostToHistory(BlogPost existingBlogPost) {
-        return new BlogPostHistory(null, existingBlogPost.getId(),
-                existingBlogPost.getTitle(),
-                existingBlogPost.getBody(),
-                existingBlogPost.getAuthor(),
-                existingBlogPost.getPublicationDate(),
-                existingBlogPost.getBlogPostStatus(),
-                new ArrayList<>(existingBlogPost.getCategories()),
-                existingBlogPost.getVersion());
-    }
-
-    public BlogPost getUpdatedBlogPost(String newTitle, String newBody, BlogPost existingBlogPost) {
-        BlogPost updatedBlogPost = new BlogPost();
-
-        updatedBlogPost.setTitle(newTitle != null && !newTitle.isBlank() ? newTitle : existingBlogPost.getTitle());
-        updatedBlogPost.setBody(newBody != null && !newBody.isBlank() ? newBody : existingBlogPost.getBody());
-
-        updatedBlogPost.setVersion(existingBlogPost.getVersion() + 1);
-        updatedBlogPost.setPublicationDate(existingBlogPost.getPublicationDate());
-        updatedBlogPost.setBlogPostStatus(existingBlogPost.getBlogPostStatus());
-        updatedBlogPost.setCategories(existingBlogPost.getCategories());
-        updatedBlogPost.setAuthor(existingBlogPost.getAuthor());
-        updatedBlogPost.setId(existingBlogPost.getId());
-        return updatedBlogPost;
-    }
-
-    public BlogPost convertToEntity(BlogPostRequestDTO blogPostRequestDTO) {
-
-        List<Category> categories = blogPostRequestDTO.getCategoriesDTO().stream()
-                                                      .map(categoryDTO -> applicationMediator.resolveCategoryByName(
-                                                              categoryDTO.getCategoryName())).toList();
-
-        return new BlogPost(null,
-                blogPostRequestDTO.getTitle(),
-                blogPostRequestDTO.getBody(),
-                blogPostRequestDTO.getAuthor(),
-                LocalDate.now(),
-                BlogPostStatus.DRAFT,
-                categories,
-                1);
-    }
-
-    public BlogPostResponseDTO convertToResponseDTO(BlogPost blogPost) {
-        List<CategoryDTO> categoryDTOs = blogPost.getCategories().stream()
-                                                 .map(category -> new CategoryDTO(category.getCategoryName())).toList();
-
-        return new BlogPostResponseDTO(blogPost.getId(),
-                blogPost.getTitle(),
-                blogPost.getBody(),
-                blogPost.getAuthor(),
-                blogPost.getPublicationDate(),
-                blogPost.getBlogPostStatus(),
-                categoryDTOs);
-    }
-
     public List<BlogPostResponseDTO> getBlogPostsByCategory(String categoryName) {
         List<BlogPost> filteredBlogPosts = blogPostRepository.findByCategories_CategoryNameIgnoreCase(categoryName);
 
@@ -128,5 +72,61 @@ public class BlogPostService {
                                                       .orElseThrow(() -> new BlogPostIdNotFoundException(id));
         existingBlogPost.setBlogPostStatus(blogPostStatus);
         blogPostRepository.save(existingBlogPost);
+    }
+
+    private BlogPost convertToEntity(BlogPostRequestDTO blogPostRequestDTO) {
+
+        List<Category> categories = blogPostRequestDTO.getCategoriesDTO().stream()
+                                                      .map(categoryDTO -> applicationMediator.resolveCategoryByName(
+                                                              categoryDTO.getCategoryName())).toList();
+
+        return new BlogPost(null,
+                blogPostRequestDTO.getTitle(),
+                blogPostRequestDTO.getBody(),
+                blogPostRequestDTO.getAuthor(),
+                LocalDate.now(),
+                BlogPostStatus.DRAFT,
+                categories,
+                1);
+    }
+
+    private BlogPostResponseDTO convertToResponseDTO(BlogPost blogPost) {
+        List<CategoryDTO> categoryDTOs = blogPost.getCategories().stream()
+                                                 .map(category -> new CategoryDTO(category.getCategoryName())).toList();
+
+        return new BlogPostResponseDTO(blogPost.getId(),
+                blogPost.getTitle(),
+                blogPost.getBody(),
+                blogPost.getAuthor(),
+                blogPost.getPublicationDate(),
+                blogPost.getBlogPostStatus(),
+                categoryDTOs);
+    }
+
+    private BlogPostHistory convertBlogPostToHistory(BlogPost existingBlogPost) {
+        return new BlogPostHistory(null,
+                existingBlogPost.getId(),
+                existingBlogPost.getTitle(),
+                existingBlogPost.getBody(),
+                existingBlogPost.getAuthor(),
+                existingBlogPost.getPublicationDate(),
+                existingBlogPost.getBlogPostStatus(),
+                new ArrayList<>(existingBlogPost.getCategories()),
+                existingBlogPost.getVersion());
+    }
+
+    private BlogPost getUpdatedBlogPost(String newTitle, String newBody, BlogPost existingBlogPost) {
+        BlogPost updatedBlogPost = new BlogPost();
+
+        updatedBlogPost.setTitle(newTitle != null && !newTitle.isBlank() ? newTitle : existingBlogPost.getTitle());
+        updatedBlogPost.setBody(newBody != null && !newBody.isBlank() ? newBody : existingBlogPost.getBody());
+
+        updatedBlogPost.setVersion(existingBlogPost.getVersion() + 1);
+        updatedBlogPost.setPublicationDate(existingBlogPost.getPublicationDate());
+        updatedBlogPost.setBlogPostStatus(existingBlogPost.getBlogPostStatus());
+        updatedBlogPost.setCategories(existingBlogPost.getCategories());
+        updatedBlogPost.setAuthor(existingBlogPost.getAuthor());
+        updatedBlogPost.setId(existingBlogPost.getId());
+        return updatedBlogPost;
     }
 }
